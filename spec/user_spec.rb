9077b27cf76f4518b5ff6e8e0a63f058d9135c8f
Nozomi Tanaka <barasixi@gmail.com>
2016-09-29 00:37:32 +0900

    refector

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,16 +1,20 @@
 require 'spec_helper'
 
 RSpec.describe User do
+  before do
+    @params = {name: 'user'}
+  end
+
   describe '#greet' do
     context '12歳以下であれば' do
       it 'ひらがなで答える' do
-        user = User.new(name: 'user', age: 12)
+        user = User.new(@params.merge(age: 12))
         expect(user.greet).to eq 'ぼくはuserだよ。'
       end
     end
     context '13歳以上であれば' do
       it '漢字で答える' do
-        user = User.new(name: 'user', age: 13)
+        user = User.new(@params.merge(age: 13))
         expect(user.greet).to eq '僕はuserです。'
       end
     end
